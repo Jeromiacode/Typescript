4abd22cd42520ad1e2ceb9862d10b27fd38966ae
Jeromia <depuytjerome@gmail.com>
2022-08-15 19:54:29 +0200

    first commit

---
+++ intro/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let sales = 123456789523;
+let course = 'Typescript';
+let is_published = true;
+const numbers = [];
+//# sourceMappingURL=index.js.map
---
+++ intro/src/index.ts
@@ -0,0 +1,9 @@
+let sales = 123_456_789_523;
+let course = 'Typescript';
+let is_published = true;
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+const numbers: string[] = []
